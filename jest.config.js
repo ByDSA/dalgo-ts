417f7876557b027100f82552bb0b1df97fbe70cc
Daniel Sales <danisales.es@gmail.com>
2023-04-19 21:14:30 +0200

    chore: misc

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   globals: {
   },
   moduleNameMapper: {
-    "^#src($|/.*)$": "<rootDir>/src/$1",
+    "^~($|/.*)$": "<rootDir>/src/$1",
     "^#tests($|/.*)$": "<rootDir>/tests/$1",
   },
 };
