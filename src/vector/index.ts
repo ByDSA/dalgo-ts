1deccdd19f90cd8adbad86f342029852a0ae8a8b
Daniel Sales <danisales.es@gmail.com>
2023-04-20 17:05:36 +0200

    chore!: exponer el stringify de vector

--- src/vector/index.ts
+++ src/vector/index.ts
@@ -1,8 +1,10 @@
 import { Vector1, Vector2, Vector3, VectorN } from "./Vector";
+import { stringify } from "./stringify";
 
 export {
   VectorN,
   Vector2,
   Vector3,
   Vector1,
+  stringify as vectorStringify,
 };
--- src/vector/tests/utils.ts
+++ src/vector/tests/utils.ts
@@ -1,22 +1,11 @@
-import { Vector2, VectorN } from "../Vector";
+/* eslint-disable import/prefer-default-export */
+import { VectorN } from "../Vector";
+import { stringify } from "../stringify";
 
 export function expectVector<D extends number>(actual: VectorN<D>, expected: VectorN<D>) {
   if (!actual || !expected)
     throw new Error("position is undefined");
 
   if (actual[0] !== expected[0] || actual[1] !== expected[1])
-    throw new Error(`${vectorStringify(actual)} is not ${vectorStringify(expected)}`);
-}
-
-export function vector2Stringify(position: Vector2) {
-  return `{${position[0]}, ${position[1]}}`;
-}
-
-export function vectorStringify<D extends number>(position: VectorN<D>) {
-  if (position.length === 2)
-    return vector2Stringify(position as Vector2);
-
-  const listStr = position.reduce((acc, value) => `${acc}, ${value}`, "");
-
-  return `{${listStr}}`;
+    throw new Error(`${stringify(actual)} is not ${stringify(expected)}`);
 }
