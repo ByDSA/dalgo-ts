d96b8ca1be1757c7f5ec994f9bffe5ea53101321
Daniel Sales <danisales.es@gmail.com>
2023-04-20 17:05:36 +0200

    chore!: exponer el stringify de vector

---
+++ src/vector/stringify.ts
@@ -0,0 +1,14 @@
+import { Vector2, VectorN } from "./Vector";
+
+export function stringify2(position: Vector2) {
+  return `{${position[0]}, ${position[1]}}`;
+}
+
+export function stringify<D extends number>(position: VectorN<D>) {
+  if (position.length === 2)
+    return stringify2(position as Vector2);
+
+  const listStr = position.reduce((acc, value) => `${acc}, ${value}`, "");
+
+  return `{${listStr}}`;
+}
